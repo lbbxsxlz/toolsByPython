cc6b94e20efa7d24257f2550817ea35b632aaf39
BinbinLee <lbbxsxlz@gmail.com>
2022-08-12 14:30:36 +0800

    Create conv-file.py

---
+++ conv-file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# a script to convert file encoding
+
+import os
+import sys
+
+path = sys.argv[1]
+
+def changeEncoding(filePath):
+    print filePath
+    tempName = filePath+'.m'
+    print tempName
+
+    inputFile = open(filePath,'rb')
+    content = unicode(inputFile.read(),'ascii')
+    inputFile.close()
+
+    outputFile = open(tempName,'wb')
+    outputFile.write(content.encode('utf-16'))
+    outputFile.close()
+
+    #os.rename(tempName,filePath)
+
+def fileFilter(dummyArg, thisDir, dirChildrenList):
+    for child in dirChildrenList:
+        changeEncoding(thisDir+'/'+child)
+
+os.path.walk(path, fileFilter, None)
