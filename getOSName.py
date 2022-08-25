f68cc280256e49712fd8387b21e47da2bfaf5c95
BinbinLee <lbbxsxlz@gmail.com>
2022-08-25 19:47:54 +0800

    add get os name

---
+++ getOSName.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# diff file
+__author__ = 'lbbxsxlz@gmail.com'
+
+import platform
+
+def get_os_name():
+    sys = platform.system()
+    if sys == "Windows":
+        print("OS is Windows!")
+    elif sys == "Linux":
+        print("OS is Linux!")
+
+
+if __name__ == "__main__":
+    get_os_name()
+
